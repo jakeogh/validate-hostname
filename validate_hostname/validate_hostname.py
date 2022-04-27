6703cbab62638ad2c8cb1f9b372fc29cfaff1ce5
jakeogh <github.com@v6y.net>
2022-04-26 17:29:28 -0700

    auto-commit

--- validate_hostname/validate_hostname.py
+++ validate_hostname/validate_hostname.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
-# flake8: noqa           # flake8 has no per file settings :(
 # pylint: disable=C0111  # docstrings are always outdated and wrong
 # pylint: disable=W0511  # todo is encouraged
 # pylint: disable=C0301  # line too long
@@ -20,24 +19,6 @@
 # pylint: disable=R0916  # Too many boolean expressions in if statement
 # pylint: disable=C0305  # Trailing newlines editor should fix automatically, pointless warning
 
-# code style:
-#   no guessing on spelling: never tmp_X always temporary_X
-#   no guessing on case: local vars, functions and methods are lower case. classes are ThisClass(). Globals are THIS.
-#   del vars explicitely ASAP, assumptions are buggy
-#   rely on the compiler, code verbosity and explicitness can only be overruled by benchamrks (are really compiler bugs)
-#   no tabs. code must display the same independent of viewer
-#   no recursion, recursion is undecidiable, randomly bounded, and hard to reason about
-#   each elementis the same, no special cases for the first or last elemetnt:
-#       [1, 2, 3,] not [1, 2, 3]
-#       def this(*.
-#                a: bool,
-#                b: bool,
-#               ):
-#
-#   expicit loop control is better than while (condition):
-#       while True:
-#           # continue/break explicit logic
-
 
 # TODO:
 #   https://github.com/kvesteri/validators
@@ -50,7 +31,7 @@ from signal import signal
 
 import click
 
-signal(SIGPIPE,SIG_DFL)
+signal(SIGPIPE, SIG_DFL)
 from pathlib import Path
 from typing import ByteString
 from typing import Generator
@@ -59,81 +40,34 @@ from typing import List
 from typing import Optional
 from typing import Sequence
 from typing import Tuple
+from typing import Union
 
-#from with_sshfs import sshfs
-#from with_chdir import chdir
-from asserttool import nevd
-from enumerate_input import enumerate_input
-from retry_on_exception import retry_on_exception
+from asserttool import ic
+from clicktool import click_add_options
+from clicktool import click_global_options
+from clicktool import tv
+from eprint import eprint
+from mptool import unmp
 
-#from collections import defaultdict
-#from prettyprinter import cpprint, install_extras
-#install_extras(['attrs'])
-
-#from kcl.configops import click_read_config
-#from kcl.configops import click_write_config_entry
-
-#from asserttool import not_root
-#from pathtool import path_is_block_special
-#from getdents import files
-#from prettytable import PrettyTable
-#output_table = PrettyTable()
-
-
-
-def eprint(*args, **kwargs):
-    if 'file' in kwargs.keys():
-        kwargs.pop('file')
-    print(*args, file=sys.stderr, **kwargs)
-
-
-try:
-    from icecream import ic  # https://github.com/gruns/icecream
-    from icecream import icr  # https://github.com/jakeogh/icecream
-except ImportError:
-    ic = eprint
-    icr = eprint
-
-
-def validate_slice(slice_syntax):
-    assert isinstance(slice_syntax, str)
-    for c in slice_syntax:
-        if c not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '[', ']', ':']:
-            raise ValueError(slice_syntax)
-    return slice_syntax
+# from asserttool import validate_slice
 
 
 @click.command()
 @click.argument("hostnames", type=str, nargs=-1)
-@click.option('--verbose', is_flag=True)
-@click.option('--debug', is_flag=True)
+@click.option("--verbose", is_flag=True)
+@click_add_options(click_global_options)
 @click.pass_context
-def cli(ctx,
-        hostnames: Optional(Iterable[str]),
-        verbose: bool,
-        debug: bool,
-        ):
-
-    ctx.ensure_object(dict)
-    null, end, verbose, debug = nevd(ctx=ctx,
-                                     printn=False,
-                                     ipython=False,
-                                     verbose=verbose,
-                                     debug=debug,)
+def cli(
+    ctx,
+    hostnames: Sequence[str],
+    verbose: Union[bool, int, float],
+    verbose_inf: bool,
+    dict_input: bool,
+):
 
     iterator = hostnames
 
     index = 0
-    for index, hostname in enumerate_input(iterator=iterator,
-                                           dont_decode=False,  # hostnames
-                                           null=null,
-                                           progress=False,
-                                           skip=skip,
-                                           head=head,
-                                           tail=tail,
-                                           debug=debug,
-                                           verbose=verbose,):
-
+    for index, hostname in enumerate(iterator):
         if verbose:
             ic(index, hostname)
-
